dfc12e8da097d599997590d995da12d6fb04e767
Gary <gary.odonoghue@gmail.com>
2018-01-25 19:11:32 +0000

    Add screen for setting reminders

---
+++ app/src/main/java/com/gary/spiders/util/ReminderScreen.java
@@ -0,0 +1,15 @@
+package com.gary.spiders.util;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gary.spiders.R;
+
+public class ReminderScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_screen);
+    }
+}
