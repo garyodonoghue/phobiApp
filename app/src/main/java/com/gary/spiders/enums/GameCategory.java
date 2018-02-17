144f81c9b89c449f42594ccb2a93282dbc9bddcc
Gary <gary.odonoghue@gmail.com>
2018-02-17 18:16:12 +0000

    Add the new categories based on the images, need to readjust the level system to accomodate more levels

--- app/src/main/java/com/gary/spiders/enums/GameCategory.java
+++ app/src/main/java/com/gary/spiders/enums/GameCategory.java
@@ -7,8 +7,9 @@ package com.gary.spiders.enums;
 public enum GameCategory {
     LINGUISTIC_LOW, LINGUISTIC_MED, LINGUISTIC_HIGH, CARTOON_LOW, CARTOON_MED, CARTOON_HIGH,
     DRAWINGS_BW_LOW, DRAWINGS_BW_MED, DRAWINGS_BW_HIGH, DRAWING_COL_LOW, DRAWING_COL_MED, DRAWING_COL_HIGH,
-    PHOTOS_SMALL_LOW, PHOTOS_SMALL_MED, PHOTOS_SMALL_HIGH, PHOTOS_BIG_LOW, PHOTOS_BIG_MED, PHOTOS_BIG_HIGH,
-    VIDEOS_LOW, VIDEOS_MED, VIDEOS_HIGH, IMAGE_PICKER_SPIDER_IMAGES, IMAGE_PICKER_NON_SPIDER_BW_IMAGES, IMAGE_PICKER_SPIDER_BW_IMAGES;
+    PHOTOS_BW_LOW, PHOTOS_BW_MED, PHOTOS_BW_HIGH, PHOTOS_COL_SMALL_LOW, PHOTOS_COL_SMALL_MED, PHOTOS_COL_SMALL_HIGH,
+    PHOTOS_COL_BIG_LOW, PHOTOS_COL_BIG_MED, PHOTOS_COL_BIG_HIGH, VIDEOS_LOW, VIDEOS_MED, VIDEOS_HIGH, IMAGE_PICKER_SPIDER_IMAGES,
+    IMAGE_PICKER_NON_SPIDER_BW_IMAGES, IMAGE_PICKER_SPIDER_BW_IMAGES;
 
     public boolean isLinguistic(){
         return this.toString().contains("LINGUISTIC");
@@ -76,39 +77,47 @@ public enum GameCategory {
             gameLevel = GameCategory.DRAWING_COL_HIGH;
         }
         else if(userLevel > 45 && userLevel <= 48){
-            gameLevel = GameCategory.PHOTOS_SMALL_LOW;
+            gameLevel = GameCategory.PHOTOS_BW_LOW;
         }
         else if(userLevel > 48 && userLevel <= 54){
-            gameLevel = GameCategory.PHOTOS_SMALL_MED;
+            gameLevel = GameCategory.PHOTOS_BW_MED;
         }
         else if(userLevel > 54 && userLevel <= 60){
-            gameLevel = GameCategory.PHOTOS_SMALL_HIGH;
+            gameLevel = GameCategory.PHOTOS_BW_HIGH;
         }
         else if(userLevel > 60 && userLevel <= 63){
-            gameLevel = GameCategory.PHOTOS_BIG_LOW;
+            gameLevel = GameCategory.PHOTOS_COL_SMALL_LOW;
         }
         else if(userLevel > 63 && userLevel <= 69){
-            gameLevel = GameCategory.PHOTOS_BIG_MED;
+            gameLevel = GameCategory.PHOTOS_COL_SMALL_MED;
         }
         else if(userLevel > 69 && userLevel <= 75){
-            gameLevel = GameCategory.PHOTOS_BIG_HIGH;
+            gameLevel = GameCategory.PHOTOS_COL_SMALL_HIGH;
         }
         else if(userLevel > 75 && userLevel <= 82){
-            gameLevel = GameCategory.VIDEOS_LOW;
+            gameLevel = GameCategory.PHOTOS_COL_BIG_LOW;
         }
         else if(userLevel > 82 && userLevel <= 90){
+            gameLevel = GameCategory.PHOTOS_COL_BIG_MED;
+        }
+        else if(userLevel > 90 && userLevel <= 100){
+            gameLevel = GameCategory.PHOTOS_COL_BIG_HIGH;
+        }
+        else if(userLevel > 90 && userLevel <= 100){
+            gameLevel = GameCategory.VIDEOS_LOW;
+        }
+        else if(userLevel > 90 && userLevel <= 100){
             gameLevel = GameCategory.VIDEOS_MED;
         }
         else if(userLevel > 90 && userLevel <= 100){
             gameLevel = GameCategory.VIDEOS_HIGH;
         }
-
         return gameLevel;
     }
 
     public static GameCategory[] getInitialAssessmentCategories(){
         return new GameCategory[]{LINGUISTIC_HIGH, CARTOON_HIGH, DRAWINGS_BW_HIGH,
-                DRAWING_COL_HIGH, PHOTOS_SMALL_HIGH, PHOTOS_BIG_HIGH, VIDEOS_HIGH };
+                DRAWING_COL_HIGH, PHOTOS_BW_HIGH, PHOTOS_COL_SMALL_HIGH, PHOTOS_COL_BIG_HIGH, VIDEOS_HIGH };
     }
 
     public int getBeginnerLevelForCategory(){
@@ -117,8 +126,9 @@ public enum GameCategory {
             case CARTOON_HIGH:return 6;
             case DRAWINGS_BW_HIGH: return 16;
             case DRAWING_COL_HIGH:return 31;
-            case PHOTOS_SMALL_HIGH:return 46;
-            case PHOTOS_BIG_HIGH:return 61;
+            case PHOTOS_BW_HIGH:return 46;
+            case PHOTOS_COL_SMALL_HIGH:return 61;
+            case PHOTOS_COL_BIG_HIGH:return 61; // TODO Readjust all the levels
             case VIDEOS_HIGH:return 76;
             default:return 0;
         }
